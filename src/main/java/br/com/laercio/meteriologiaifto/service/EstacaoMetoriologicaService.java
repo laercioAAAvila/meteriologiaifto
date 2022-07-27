6f3b8997b5e2fd41651f7e3dd502c67ec0ef6584
cassiaavila <laercio.aaavila@gmail.com>
2022-07-27 13:53:03 -0300

    add controller for estação

--- src/main/java/br/com/laercio/meteriologiaifto/service/EstacaoMetoriologicaService.java
+++ src/main/java/br/com/laercio/meteriologiaifto/service/EstacaoMetoriologicaService.java
@@ -1,9 +1,11 @@
 package br.com.laercio.meteriologiaifto.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import br.com.laercio.meteriologiaifto.model.EstacaoMeteriologica;
 
 public interface EstacaoMetoriologicaService {
 	public List<EstacaoMeteriologica> findAll();
+	Optional<EstacaoMeteriologica> findById(long id);
 }
